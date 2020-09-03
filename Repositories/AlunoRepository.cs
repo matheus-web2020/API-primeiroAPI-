1b408e50bebb53dc73a8dfbecc6526f08522eaf8
Matheus <matheus.fusca.lima@outlook.com>
2020-09-03 16:50:13 -0300

    Boletim final

---
+++ Repositories/AlunoRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using APIBoletim.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APIBoletim.Domains;
+
+namespace APIBoletim.Repositories
+{
+    public class AlunoRepository : IAluno
+    {
+        public Aluno Alterar(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno BuscarPorId(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno Cadastrar(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno Excluir(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Aluno> LerTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
